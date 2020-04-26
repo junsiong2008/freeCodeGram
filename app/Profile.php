353f074230ea8634b9bfd672dd1d782b0317b14a
teng <junsiong2599@gmail.com>
2020-04-26 15:09:45 +0800

    initial commit

---
+++ app/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+    
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : "/profile/6qkD0oa2fjHctCA22E2p1wqGV2tMdiX4tik4RQ4L.jpeg";
+    
+
+        return '/storage/'.$imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
